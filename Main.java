c6e53d264eabdadc234647c782d87772899b1aa5
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-07 23:37:10 +0530

    Initialising duck instances in main

--- Main.java
+++ Main.java
@@ -1,1 +1,7 @@
-
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+        Duck mallard = new MallardDuck();
+        Duck redhead = new RedheadDuck();
+        Duck rubber = new RubberDuck();
+        Duck decoy = new DecoyDuck();
