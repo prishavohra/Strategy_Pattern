41e1667c178ba1825adaac369d1661b2f5d7a720
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-07 23:25:09 +0530

    Update RedheadDuck.java

--- RedheadDuck.java
+++ RedheadDuck.java
@@ -1,1 +1,9 @@
-
+// RedheadDuck.java
+class RedheadDuck extends Duck {
+    public RedheadDuck() {
+        flyBehavior = new FlyWithWings();
+        quackBehavior = new Quack();
+        swimBehavior = new Swim();
+    }
+    public void display() { System.out.println("I am a Redhead Duck."); }
+}
