5923355f1762b66f503ac06a4c21e97cde5e0cd3
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-07 23:12:03 +0530

    Implementing FlyBehavior.java

--- FlyBehavior.java
+++ FlyBehavior.java
@@ -1,1 +1,4 @@
-
+// FlyBehavior.java
+interface FlyBehavior { void fly(); }
+class FlyWithWings implements FlyBehavior { public void fly() { System.out.println("I'm flying with wings!"); } }
+class FlyNoWay implements FlyBehavior { public void fly() { System.out.println("I can't fly."); } }
