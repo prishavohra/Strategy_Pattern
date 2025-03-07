7b8de0926f3cbbc62f9f9efae1813a411d935bb6
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-07 23:23:57 +0530

    Update MallardDuck.java

--- MallardDuck.java
+++ MallardDuck.java
@@ -1,1 +1,9 @@
-
+// MallardDuck.java
+class MallardDuck extends Duck {
+    public MallardDuck() {
+        flyBehavior = new FlyWithWings();
+        quackBehavior = new Quack();
+        swimBehavior = new Swim();
+    }
+    public void display() { System.out.println("I am a Mallard Duck."); }
+}
