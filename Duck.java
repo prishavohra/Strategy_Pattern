c712d81e21b0d84650fcc0d5456a37cd5ccf7023
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-07 23:08:31 +0530

    Calling duck behaviors

--- Duck.java
+++ Duck.java
@@ -3,3 +3,13 @@ abstract class Duck {
     FlyBehavior flyBehavior;
     QuackBehavior quackBehavior;
     SwimBehavior swimBehavior;
+
+    public void setFlyBehavior(FlyBehavior fb) { flyBehavior = fb; }
+    public void setQuackBehavior(QuackBehavior qb) { quackBehavior = qb; }
+    public void setSwimBehavior(SwimBehavior sb) { swimBehavior = sb; }
+
+    abstract void display();
+    public void performFly() { flyBehavior.fly(); }
+    public void performQuack() { quackBehavior.quack(); }
+    public void performSwim() { swimBehavior.swim(); }
+}
