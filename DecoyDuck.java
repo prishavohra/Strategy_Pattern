b5570a18ad408bacc3b37892df83f7ff70b9cfaa
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-07 23:28:09 +0530

    Update DecoyDuck.java

--- DecoyDuck.java
+++ DecoyDuck.java
@@ -1,1 +1,9 @@
-
+// DecoyDuck.java
+class DecoyDuck extends Duck {
+    public DecoyDuck() {
+        flyBehavior = new FlyNoWay();
+        quackBehavior = new MuteQuack();
+        swimBehavior = new Drown();
+    }
+    public void display() { System.out.println("I am a Decoy Duck."); }
+}
