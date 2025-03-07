9ba8e0da047575c6026c2f2e2cc0aab8a176feea
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-07 23:20:39 +0530

    Implementing QuackBehavior.java

--- QuackBehavior.java
+++ QuackBehavior.java
@@ -1,1 +1,5 @@
-
+// QuackBehavior.java
+interface QuackBehavior { void quack(); }
+class Quack implements QuackBehavior { public void quack() { System.out.println("Quack Quack!"); } }
+class Squeak implements QuackBehavior { public void quack() { System.out.println("Squeak Squeak!"); } }
+class MuteQuack implements QuackBehavior { public void quack() { System.out.println("..."); } }
