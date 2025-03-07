5528373ef86b88c86dacc307e30cd8880ea1112b
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-07 23:26:58 +0530

    Update RubberDuck.java

--- RubberDuck.java
+++ RubberDuck.java
@@ -1,1 +1,9 @@
-
+// RubberDuck.java
+class RubberDuck extends Duck {
+    public RubberDuck() {
+        flyBehavior = new FlyNoWay();
+        quackBehavior = new Squeak();
+        swimBehavior = new Float();
+    }
+    public void display() { System.out.println("I am a Rubber Duck."); }
+}
